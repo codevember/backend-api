49f91d8f6e0fdd3a04b8c5bba256b38b954d313e
Valentin Daguenet <valentin.daguenet@gmail.com>
2018-09-28 09:28:51 +0200

    update dependencies

--- dist/browser.js
+++ dist/browser.js
@@ -1,12 +1,14 @@
-'use strict';
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 var firebase = require('firebase');
 
-var Api = function () {
+var Api =
+/*#__PURE__*/
+function () {
   function Api() {
     _classCallCheck(this, Api);
 
@@ -18,18 +20,17 @@ var Api = function () {
   }
 
   _createClass(Api, [{
-    key: 'init',
+    key: "init",
     value: function init(apiKey, authDomain, dbName) {
       this.apiKey = apiKey;
       this.authDomain = authDomain;
-      this.url = 'https://' + dbName + '.firebaseio.com/';
+      this.url = "https://".concat(dbName, ".firebaseio.com/");
       this.dbName = dbName;
       this.user = undefined;
-
       return this.initFirebase();
     }
   }, {
-    key: 'initFirebase',
+    key: "initFirebase",
     value: function initFirebase() {
       var _this = this;
 
@@ -39,9 +40,7 @@ var Api = function () {
           authDomain: _this.authDomain,
           databaseURL: _this.url
         });
-
         _this.db = firebase.database();
-
         firebase.auth().onAuthStateChanged(function (user) {
           if (user) {
             _this.user = user;
@@ -54,18 +53,16 @@ var Api = function () {
       });
     }
   }, {
-    key: 'checkExistence',
+    key: "checkExistence",
     value: function checkExistence(year, link) {
       var _this2 = this;
 
       return new Promise(function (resolve, reject) {
-        _this2.db.ref(year + '/contributions').once('value').then(function (snapshot) {
+        _this2.db.ref("".concat(year, "/contributions")).once('value').then(function (snapshot) {
           var exists = false;
-
           var testLink = link.replace('https://', 'http://');
           testLink = testLink.replace('/pen/', '/full/');
           testLink = testLink.replace('/details/', '/full/');
-
           snapshot.forEach(function (data) {
             var dataUrl = data.val().url.replace('https://', 'http://');
             dataUrl = dataUrl.replace('/pen/', '/full/');
@@ -75,13 +72,12 @@ var Api = function () {
               exists = true;
             }
           });
-
           resolve(exists);
         });
       });
     }
   }, {
-    key: 'saveContribution',
+    key: "saveContribution",
     value: function saveContribution(value) {
       var _this3 = this;
 
@@ -94,7 +90,9 @@ var Api = function () {
         _this3.checkExistence(value.year, value.url).then(function (exists) {
           if (exists === false) {
             value.slug = _this3.generateSlug(value);
-            var newContrib = _this3.db.ref(value.year + '/contributions').push();
+
+            var newContrib = _this3.db.ref("".concat(value.year, "/contributions")).push();
+
             newContrib.set(value);
           }
 
@@ -103,25 +101,26 @@ var Api = function () {
       });
     }
   }, {
-    key: 'generateSlug',
+    key: "generateSlug",
     value: function generateSlug(value) {
       var slug = '';
       var strDay = value.day;
-      if (value.day < 10) {
-        strDay = '0' + value.day;
-      }
-      slug += value.year + ' ' + strDay + ' ' + value.title + ' ' + value.author;
-      slug = slug.replace(/\s/g, '-').toLowerCase();
 
+      if (value.day < 10) {
+        strDay = "0".concat(value.day);
+      }
+
+      slug += "".concat(value.year, " ").concat(strDay, " ").concat(value.title, " ").concat(value.author);
+      slug = slug.replace(/\s/g, '-').toLowerCase();
       return slug;
     }
   }, {
-    key: 'getContributionsOfDay',
+    key: "getContributionsOfDay",
     value: function getContributionsOfDay(year, day) {
       var _this4 = this;
 
       return new Promise(function (resolve, reject) {
-        _this4.db.ref(year + '/contributions').orderByChild('day').equalTo(day).once('value').then(function (snapshot) {
+        _this4.db.ref("".concat(year, "/contributions")).orderByChild('day').equalTo(day).once('value').then(function (snapshot) {
           var contribs = [];
           snapshot.forEach(function (data) {
             contribs.push(data.val());
@@ -131,7 +130,7 @@ var Api = function () {
       });
     }
   }, {
-    key: 'getCurrentUser',
+    key: "getCurrentUser",
     value: function getCurrentUser() {
       if (this.user) {
         return this.user;
@@ -140,12 +139,12 @@ var Api = function () {
       return firebase.auth().currentUser;
     }
   }, {
-    key: 'signin',
+    key: "signin",
     value: function signin(email, password) {
       return firebase.auth().signInWithEmailAndPassword(email, password);
     }
   }, {
-    key: 'signout',
+    key: "signout",
     value: function signout() {
       return firebase.auth().signOut();
     }
